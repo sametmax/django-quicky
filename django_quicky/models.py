c1fcfd2e1f1ddef5660391d304eb4215370d9454
sametmax <lesametlemax@gmail.com>
2013-02-20 19:44:25 +0100

    Adding a way to patch django models and an function to get client IP easily

--- django_quicky/models.py
+++ django_quicky/models.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4 nu
 
+import types
+
 from random import randint
 
 from django.shortcuts import _get_queryset
 from django.db.models import Max
+from django.db.models.fields import Field
 
 
-__all__ = ['get_random_objects', 'get_object_or_none']
+__all__ = ['get_random_objects', 'get_object_or_none', 'patch_model']
 
 
 
@@ -49,3 +52,84 @@ def get_object_or_none(klass, *args, **kwargs):
         return queryset.get(*args, **kwargs)
     except queryset.model.DoesNotExist:
         return None
+
+
+
+def patch_model(model_to_patch, class_to_patch_with):
+    """
+        Adapted from https://gist.github.com/1402045
+
+        Monkey patch a django model with additional or
+        replacement fields and methods.
+
+            - All fields and methods that didn't exist previously are added.
+
+            - Existing methods with the same names are renamed with
+              <methodname>__overridden, so there are still accessible,
+              then the new ones are added.
+
+            - Existing fields with the same name are deleted and replaced with
+              the new fields.
+
+        The class used to patch the model MUST be an old-style class (so
+        this may not work with Python 3).
+
+        Example (in your models.py):
+
+            from django.contrib.auth.models import User
+            from django_quicky.models import patch_model
+
+            class UserOverride: # we don't need to inherit from anything
+                email = models.EmailField(_('e-mail address'), unique=True)
+                new_field = models.CharField(_('new field'), max_length=10)
+
+                def save(self, *args, **kwargs):
+
+                    # Call original save() method
+                    self.save__overridden(*args, **kwargs)
+
+                    # More custom save
+
+            patch_model(User, UserOverride)
+
+    """
+
+    # The _meta attribute is where the definition of the fields is stored in
+    # django model classes.
+    patched_meta = getattr(model_to_patch, '_meta')
+    field_lists = (patched_meta.local_fields, patched_meta.local_many_to_many)
+
+    for name, obj in class_to_patch_with.__dict__.iteritems():
+
+        # If the attribute is a field, delete any field with the same name.
+        if isinstance(obj, Field):
+
+            for field_list in field_lists:
+
+                match = ((i, f) for i, f in enumerate(field_list) if f.name == name)
+                try:
+                    i, field = match.next()
+                    # The creation_counter is used by django to know in
+                    # which order the database columns are declared. We
+                    # get it to ensure that when we override a field it
+                    # will be declared in the same position as before.
+                    obj.creation_counter = field.creation_counter
+                    field_list.pop(i)
+                finally:
+                    break
+
+        # Add "__overridden" to method names if they already exist.
+        elif isinstance(obj, (types.FunctionType, property,
+                               staticmethod, classmethod)):
+
+            # rename the potential old method
+            attr = getattr(model_to_patch, name, None)
+            if attr:
+                setattr(model_to_patch, name + '__overridden', attr)
+
+            # bind the new method to the object
+            if isinstance(obj, types.FunctionType):
+                obj = types.UnboundMethodType(obj, None, model_to_patch)
+
+        # Add the new field/method name and object to the model.
+        model_to_patch.add_to_class(name, obj)
--- django_quicky/utils.py
+++ django_quicky/utils.py
@@ -23,3 +23,12 @@ def setting(name, default):
     return getattr(settings, name, default)
 
 
+def get_client_ip(request):
+    """
+        Return the client IP address as a string.
+    """
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        return x_forwarded_for.split(',')[0]
+
+    return request.META.get('REMOTE_ADDR')
